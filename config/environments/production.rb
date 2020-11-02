2892d408f77ae38df80220e843aa67bbffbbd33a
Can Talas <can.talas@infopark.de>
2020-11-02 17:03:16 +0100

    domain in prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -127,7 +127,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "young-chamber-46645.herokuapp.com/",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
