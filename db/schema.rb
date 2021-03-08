65212abb1c4f52fd6a963a05a67df281ed780f04
Can Talas <can.talas@infopark.de>
2021-03-08 12:01:55 +0100

    seeds eingefügt

--- db/schema.rb
+++ db/schema.rb
@@ -58,18 +58,6 @@ ActiveRecord::Schema.define(version: 2021_02_24_114500) do
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "taggings", force: :cascade do |t|
     t.bigint "micropost_id"
     t.bigint "tag_id"
@@ -93,7 +81,6 @@ ActiveRecord::Schema.define(version: 2021_02_24_114500) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "sign_in_count"
     t.string "defaultpw"
     t.string "avatar_url"
     t.integer "role"
