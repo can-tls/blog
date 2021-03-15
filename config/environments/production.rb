127e428b8179432e760dd2e964e398e9447e60b6
Can Talas <can.talas@infopark.de>
2021-03-15 16:11:08 +0100

    no message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,12 +125,11 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'young-chamber-46645.herokuapp.com',
+  :authentication => :plain,
   }
 end
